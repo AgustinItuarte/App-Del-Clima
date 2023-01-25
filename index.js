ec6f5509b6b00d9c6964388db6c97823bf74b9ea
AgustinItuarte <agustin242536@gmail.com>
2023-01-25 18:12:24 -0300

    Estructura de contenido definida

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London&APPID=3c086de3e37296dfa889363093719c81')
-.then((response) => {
-    console.log(response.json())
-})
+async function chequearClima() {
 
+    try {
+
+        let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&APPID=3c086de3e37296dfa889363093719c81')
+        let clima = await response.json()
+        console.log(clima)
+        
+    } catch (error) {
+
+        alert(error);
+
+    }
+    
+
+};
+
+chequearClima();
