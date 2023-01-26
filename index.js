6ebcab5f001e334e3878c05f627135378f900263
AgustinItuarte <agustin242536@gmail.com>
2023-01-26 09:39:11 -0300

    Iconos añadidos

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ btn.addEventListener('click', () => {
 
     let input = document.querySelector('input');
     let localidad = input.value;
-    console.log(localidad)
+
     chequearClima(localidad);
 
 })
@@ -25,7 +25,6 @@ async function chequearClima(localidad) {
         alert(error);
 
     }
-    
 
 };
 
@@ -35,16 +34,30 @@ function pupulateRightContent(clima) {
     const opcion_2 = document.querySelector('.tiempo-2')
     const opcion_3 = document.querySelector('.tiempo-3')
     const opcion_4 = document.querySelector('.tiempo-4')
+    let icon_1 = document.createElement('img');
+    let icon_2 = document.createElement('img');
+    let icon_3 = document.createElement('img');
+    let icon_4 = document.createElement('img');
 
     opcion_1.innerHTML = ''
     opcion_2.innerHTML = ''
     opcion_3.innerHTML = ''
     opcion_4.innerHTML = ''
 
-    opcion_1.innerHTML = 'Se Siente'
-    opcion_2.innerHTML = 'Humedad'
-    opcion_3.innerHTML = 'Chance de Lluvia'
-    opcion_4.innerHTML = 'Viento'
+    icon_1.src = 'icons/thermometer.svg'
+    icon_2.src = 'icons/droplet.svg'
+    icon_3.src = 'icons/cloud-drizzle.svg'
+    icon_4.src = 'icons/wind.svg'
+
+    opcion_1.appendChild(icon_1);
+    opcion_2.appendChild(icon_2);
+    opcion_3.appendChild(icon_3);
+    opcion_4.appendChild(icon_4);
+
+    opcion_1.innerHTML += 'Se Siente'
+    opcion_2.innerHTML += 'Humedad'
+    opcion_3.innerHTML += 'Chance de Lluvia'
+    opcion_4.innerHTML += 'Viento'
 
     let contenido_1 = document.createElement('h1');
     let contenido_2 = document.createElement('h1');
@@ -65,21 +78,29 @@ function pupulateRightContent(clima) {
 
 function pupulateLeftContent(clima) {
     
-    const left_content = document.querySelector('.left-content')
+    const left_content = document.querySelector('.ul-1')
 
     left_content.innerHTML = ''
 
-    let contenido_1 = document.createElement('h1');
-    let contenido_2 = document.createElement('h3');
-    let contenido_3 = document.createElement('h1');
+    let contenido_1 = document.createElement('li');
+    let contenido_2 = document.createElement('li');
+    let contenido_3 = document.createElement('li');
+    let contenido_4 = document.createElement('li');
+    let date = new Date();
+
+    contenido_1.className = 'estado'
+    contenido_2.className = 'localizacion'
+    contenido_4.className = 'temp'
     
     contenido_1.textContent = `${clima.weather[0].description}`
     contenido_2.textContent = `${clima.name}`
-    contenido_3.textContent = `${clima.main.temp}`
+    contenido_3.textContent = date
+    contenido_4.textContent = `${clima.main.temp}`
 
     left_content.appendChild(contenido_1);
     left_content.appendChild(contenido_2);
     left_content.appendChild(contenido_3);
+    left_content.appendChild(contenido_4);
 
 }
 
