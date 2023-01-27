e2c3310a510ae67e431d6b17169f3204cad18baf
AgustinItuarte <agustin242536@gmail.com>
2023-01-26 21:39:37 -0300

    console.log eliminado

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ async function chequearClima(localidad) {
         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localidad}&id=524901&lang=es&appid=3c086de3e37296dfa889363093719c81`)
         let clima = await response.json()
         
-        console.log(clima)
         pupulateRightContent(clima)
         pupulateLeftContent(clima)
 
